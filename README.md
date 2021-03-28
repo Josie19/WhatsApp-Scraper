c6eeda20ffdf267a9cbce1b9706046971d404cad
Jose Luiz Magallanes <006319894@coyote.csusb.edu>
2021-03-27 18:12:45 -0700

    WhatsApp web scraper

---
+++ README.md
@@ -0,0 +1,1 @@
+Python web scraper using selenium to retrieve raw data (HTML)
---
+++ appScrap.py
@@ -0,0 +1,20 @@
+﻿from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path='C:\\Users\\jmaga\\Desktop\\chromedriver.exe')
+driver.get('https://web.whatsapp.com/')
+
+name = input('Enter the name of user or group: ')
+msg = input('Enter your message: ')
+count = int(input('Enter the count : '))
+
+input('Enter anything after scanning QR code')
+
+user = driver.find_element_by_xpath('//span[@title = "{}"]'.format(name))
+user.click()
+
+msg_box = driver.find_element_by_class_name('_2A8P4')
+
+for i in range(count):
+    msg_box.send_keys(msg)
+    button = driver.find_element_by_class_name('_1E0Oz')
+    button.click()
